dd4bb9174c221f6fa345ed2dc8359a2b4e4c96a6
Lutsman_win <333333y@gmail.com>
2018-09-24 15:24:22 +0530

    webpack config font awesome

--- src/scripts/vendors.js
+++ src/scripts/vendors.js
@@ -4,6 +4,3 @@ import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
 // loads the Icon plugin
 UIkit.use(Icons);
-
-// import 'font-awesome-webpack';
-// import 'font-awesome/css/font-awesome.css';
